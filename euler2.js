57ecb50addb701f566821bd21ba010a8c47c2fa3
Steve Gotham <g23design@gmail.com>
2014-05-09 13:01:34 -0600

    added a solution to euler 2

---
+++ euler2.js
@@ -0,0 +1,20 @@
+function sumEvenFibocci(x)
+{
+var sequence = [1,2];
+var largest = 0;
+var sumEven = 0;
+	for (i=2;largest<x;i++)
+	{
+		sequence[i] = sequence[i-2] + sequence[i-1];
+		largest = sequence[i];
+	}
+	for (i=0;i<sequence.length;i++)
+	{
+		if ((sequence[i]%2)==0)
+		{
+			sumEven += sequence[i];
+		}
+	}
+console.log(sumEven);
+}
+sumEvenFibocci(4e6);
