44d24d71acb896ca974dbbc31b07234db05a5120
Steve Gotham <g23design@gmail.com>
2015-12-04 19:13:06 -0700

    attempting to fix my error

--- euler5.js
+++ euler5.js
@@ -1,10 +1,20 @@
+/*
+2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+*/
+
 function euler5() {
-	for (i=1;i>0;i++) {
-		if (i%1==0 && i%2==0 &&i%3==0 && i%4==0 && i%5==0 && i%6==0 && i%7==0 && i%8==0 && i%9==0 && i%10==0 && i%11==0 && i%12==0 && i%13==0 && i%14==0 && i%15==0 && i%16==0 && i%17==0 && i%18==0 && i%19==0 && i%20==0) {
-			return i;
-			break;
-		};
-	};
+	for (var i=1;i>0;i++) {
+		for (j=1;j<=20;j++) {
+			if (i%j!==0) {
+				break;
+			} else if (j == 20) {
+				return i;
+				break;
+			}
+		}
+	}
 };
 
 euler5();
